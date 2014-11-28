3c4b81c0a4d7094496b48601e299a7038acc1c8b
andrey.arnautov <puffdrew@gmail.com>
2014-11-28 18:13:41 +0200

    updated hashCode and equals based on id

--- src/main/java/myGalaxy/domain/Node.java
+++ src/main/java/myGalaxy/domain/Node.java
@@ -10,6 +10,7 @@ public class Node implements IPassable, Serializable {
 	 */
 	private static final long serialVersionUID = 2674138115435304464L;
 	private String id;
+
 	private String name;
 	private String size;
 	private String communityId;
@@ -17,56 +18,12 @@ public class Node implements IPassable, Serializable {
 	
 	public Map<String, String> additionalProperties = new HashMap<>();
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((communityId == null) ? 0 : communityId.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((size == null) ? 0 : size.hashCode());
-		return result;
-	}
-
 	public boolean isPassed() {
 		return passed;
 	}
 
 	public void setPassed(boolean passed) {
 		this.passed = passed;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (communityId == null) {
-			if (other.communityId != null)
-				return false;
-		} else if (!communityId.equals(other.communityId))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (size == null) {
-			if (other.size != null)
-				return false;
-		} else if (!size.equals(other.size))
-			return false;
-		return true;
 	}
 
 	public String getId() {
@@ -99,5 +56,35 @@ public class Node implements IPassable, Serializable {
 
 	public void setCommunityId(String communityId) {
 		this.communityId = communityId;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Node other = (Node) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
 	}
 }
