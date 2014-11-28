01ef22f2dcdc2e729c5b276b4498caca8dc3b6d1
andrey.arnautov <puffdrew@gmail.com>
2014-11-28 10:40:48 +0200

    added idnex and login page

--- src/main/java/myGalaxy/auth/BaseController.java
+++ src/main/java/myGalaxy/auth/BaseController.java
@@ -1,8 +1,5 @@
 package myGalaxy.auth;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -11,16 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/auth")
-public class AuthController {
-
+@RequestMapping("/login")
+public class BaseController {
+		
 	@RequestMapping(method = RequestMethod.GET)
-	public String getCode(ModelMap model, HttpSession session)
-			throws URISyntaxException, IOException {
-
-		System.out.println("hit!");
-
-		return "index";
+	public String getCode(ModelMap model, HttpSession session){
+		System.out.println("hot home");
+		
+		return "login";
 	}
 
 }
