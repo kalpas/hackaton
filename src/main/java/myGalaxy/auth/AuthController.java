fc17282e7ea0e6e9ea001e466c10a74045a8a770
andrey.arnautov <puffdrew@gmail.com>
2014-11-28 10:23:47 +0200

    adding spring infrastructure

--- src/main/java/myGalaxy/auth/AuthController.java
+++ src/main/java/myGalaxy/auth/AuthController.java
@@ -1,5 +1,26 @@
 package myGalaxy.auth;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/auth")
 public class AuthController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getCode(ModelMap model, HttpSession session)
+			throws URISyntaxException, IOException {
+
+		System.out.println("hit!");
+
+		return "index.jsp";
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>MyGalaxy app 4 Akvelon Hackathon</title>
+</head>
+<body>
+	Please sign in
+	<p>
+		<a
+			href="https://oauth.vk.com/authorize?client_id=4653561&scope=friends&redirect_uri=http://localhost:8080/myGalaxy/auth&display=page&v=5.27&response_type=token">Sign In</a>
+	</p>
+</body>
+</html>
