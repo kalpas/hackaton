e90cb333f7e74657b513eb91f443b602923e660f
antoxatim <antoxatim.ua@mail.ru>
2014-11-28 14:05:25 +0200

    GraphController implementation to fetch built graph

--- src/main/java/myGalaxy/mvc/GraphController.java
+++ src/main/java/myGalaxy/mvc/GraphController.java
@@ -1,17 +1,31 @@
 package myGalaxy.mvc;
 
+import javax.servlet.http.HttpSession;
+
+import myGalaxy.MyGalaxy;
+import myGalaxy.model.IGraphService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class GraphController {
-	
+
+	@Autowired
+	private IGraphService graphServ;
+
 	@RequestMapping(value = "graph")
-	public ModelAndView getGraph() {
+	public ModelAndView getGraph(
+			@RequestParam(required = true, value = MyGalaxy.SESSION_USER_ID) String userId,
+			HttpSession session) {
 		ModelAndView mav = new ModelAndView("graph");
 
+		mav.addObject("graph", this.graphServ.buildGraph(userId, String
+				.valueOf(session.getAttribute(MyGalaxy.SESSION_ACCESS_TOKEN))));
 		return mav;
 	}
 }
