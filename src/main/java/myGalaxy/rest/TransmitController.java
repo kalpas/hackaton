9d5663fb44756a3e599710001ca9f90f502806f7
antoxatim <antoxatim.ua@mail.ru>
2014-11-28 16:02:15 +0200

    Rest implementation to handle 'pull graph changes' requests

--- src/main/java/myGalaxy/rest/TransmitController.java
+++ src/main/java/myGalaxy/rest/TransmitController.java
@@ -2,13 +2,18 @@ package myGalaxy.rest;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import myGalaxy.MyGalaxy;
 import myGalaxy.domain.Edge;
+import myGalaxy.domain.Graph;
 import myGalaxy.domain.Node;
 import myGalaxy.model.IGraphService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +34,20 @@ public class TransmitController {
 	@RequestMapping(value = "/edge", method = RequestMethod.GET)
 	public List<Edge> getEdge() {
 		return this.graphServ.getEdges();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/build", method = RequestMethod.GET)
+	public Graph loadGraph(
+			@RequestParam(required = true, value = "userId") String userId,
+			HttpSession session) {
+		return this.graphServ.buildGraph(userId, String.valueOf(session
+				.getAttribute(MyGalaxy.SESSION_ACCESS_TOKEN)));
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/pull", method = RequestMethod.GET)
+	public Graph pullGraph(@RequestParam(required = true, value = "id") String id) {
+		return this.graphServ.pullGraph(id);
 	}
 }
