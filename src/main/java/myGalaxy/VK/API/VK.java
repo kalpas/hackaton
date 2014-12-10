0b061da101cbcc20eeb06f39f644bafd7b0acdf0
andrey arnautov <puffdrew@gmail.com>
2014-12-10 20:59:51 +0200

    updated redirect uris

--- src/main/java/myGalaxy/VK/API/VK.java
+++ src/main/java/myGalaxy/VK/API/VK.java
@@ -1,19 +1,16 @@
 package myGalaxy.VK.API;
 
 public class VK {
-	
-	public static final String API_HOST = "oauth.vk.com";
-	public static final String API_BASE = "api.vk.com/method/";
-	
-	public static final String AUTH_PATH = "/access_token";
-	public static final String CLIENT_SECRET = "ahX5Q63OgLkTkzAoIvDn";
-	public static final String CLIENT_ID = "4653561";
-	public static final String SCOPE = "friends";
-	public static final String AUTH_DISPLAY = "page";
-	public static final String AUTH_RESPONSE_TYPE = "code";
-	public static final String REDIRECT_URI = "http://kalpas.net/auth/vk";
-	
-	
-	
+
+    public static final String API_HOST           = "oauth.vk.com";
+    public static final String API_BASE           = "api.vk.com/method/";
+
+    public static final String AUTH_PATH          = "/access_token";
+    public static final String CLIENT_SECRET      = "ahX5Q63OgLkTkzAoIvDn";
+    public static final String CLIENT_ID          = "4653561";
+    public static final String SCOPE              = "friends";
+    public static final String AUTH_DISPLAY       = "page";
+    public static final String AUTH_RESPONSE_TYPE = "code";
+    public static final String REDIRECT_URI       = "http://galaxy.kalpas.net/auth/vk";
 
 }
--- src/main/java/myGalaxy/inst/Instagram.java
+++ src/main/java/myGalaxy/inst/Instagram.java
@@ -1,15 +1,15 @@
 package myGalaxy.inst;
 
 public final class Instagram {
-	private Instagram() {
-		
-	}
-	
-	public static final String API_HOST = "api.instagram.com/oauth";
-	
-	public static final String AUTH_PATH = "/access_token";
-	public static final String CLIENT_SECRET = "9dc5c3341b9a427caabde186b0ce7656";
-	public static final String CLIENT_ID = "9566b27c8aca4a17a004b98976af6b30";
-	public static final String REDIRECT_URI = "http://kalpas.net/auth/inst";
-	public static final String AUTH_RESPONSE_TYPE = "code";
+    private Instagram() {
+
+    }
+
+    public static final String API_HOST           = "api.instagram.com/oauth";
+
+    public static final String AUTH_PATH          = "/access_token";
+    public static final String CLIENT_SECRET      = "9dc5c3341b9a427caabde186b0ce7656";
+    public static final String CLIENT_ID          = "9566b27c8aca4a17a004b98976af6b30";
+    public static final String REDIRECT_URI       = "http://galaxy.kalpas.net/auth/inst";
+    public static final String AUTH_RESPONSE_TYPE = "code";
 }
