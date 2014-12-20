3bd1e658831e7ba8140a4d6ef4a607d85492eb06
andrey arnautov <puffdrew@gmail.com>
2014-12-20 18:33:25 +0200

    fixed config

--- src/main/java/mygalaxy/vk/api/conf/VKConfig.java
+++ src/main/java/mygalaxy/vk/api/conf/VKConfig.java
@@ -1,7 +1,5 @@
 package mygalaxy.vk.api.conf;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class VKConfig {
 
@@ -9,47 +7,54 @@ public class VKConfig {
 	public final String API_BASE         = "api.vk.com/method/";
 	public final String AUTH_PATH        = "/access_token";
 
-	@Autowired
-	@Qualifier("vkClientSecret")
 	private String      clientSecret     = "ahX5Q63OgLkTkzAoIvDn";
-	@Autowired
-	@Qualifier("vkClientId")
 	private String      clientId         = "4653561";
-	@Autowired
-	@Qualifier("vkScope")
 	private String      scope            = "friends";
-	@Autowired
-	@Qualifier("vkAuthDisplay")
 	private String      authDisplay      = "page";
-	@Autowired
-	@Qualifier("vkAuthResponseType")
 	private String      authResponseType = "code";
-	@Autowired
-	@Qualifier("vkRedirectUri")
 	private String      redirectUri      = "http://galaxy.kalpas.net/auth/vk";
 
 	public String getClientSecret() {
 		return clientSecret;
 	}
 
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
 	public String getClientId() {
 		return clientId;
 	}
 
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
 	public String getScope() {
 		return scope;
 	}
 
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
 	public String getAuthDisplay() {
 		return authDisplay;
 	}
 
+	public void setAuthDisplay(String authDisplay) {
+		this.authDisplay = authDisplay;
+	}
 	public String getAuthResponseType() {
 		return authResponseType;
 	}
 
+	public void setAuthResponseType(String authResponseType) {
+		this.authResponseType = authResponseType;
+	}
 	public String getRedirectUri() {
 		return redirectUri;
+	}
+
+	public void setRedirectUri(String redirectUri) {
+		this.redirectUri = redirectUri;
 	}
 
 }
